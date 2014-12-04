9aabda0e489639a753d6589e9d0f7923505643cb
Mike Sinclair <mm.sinclair08@gmail.com>
2014-12-04 02:42:09 -0800

    Update parseSecure.py
    Added Python Iptc rules -- now uses tool

--- parseSecure.py
+++ parseSecure.py
@@ -3,6 +3,8 @@ import time
 import re
 import subprocess
 import os
+import iptc
+
 def follow(thefile):
     thefile.seek(0,2)
     while True:
@@ -26,8 +28,19 @@ if __name__ == '__main__':
                 else:
                      	count = 0
                         ipOld = ipNew
-        if count > 4:
-                count = 0
-                ipString = str(ipNew)
-                subprocess.call(["iptables -A INPUT -s ", ipString," -j DROP"])
-                #call(["iptables -A INPUT -s ", ipString," -j DROP"]),
+       if count > 4:
+		count = 0
+		ipString = str(ipNew)
+
+		rule = iptc.Rule()
+		rule.protocol = "tcp"
+		match = iptc.Match(rule, "tcp")
+		match.dport = "22"
+		ule.add_match(match)
+		match = iptc.Match(rule, "ip")
+		match.src = ipString
+		rule.add_match(match)
+		rule.target = iptc.Target(rule, "DROP")
+		chain = iptc.Chain(iptc.Table.(iptc.Table.FILTER), "INPUT")
+		chain.insert_rule(rule)
+
